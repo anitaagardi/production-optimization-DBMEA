875ad705f8b4385ce8b5ff0f8b4dd04a9a8d8dba
Karoly Nehez <aitnehez@uni-miskolc.hu>
2020-10-14 15:49:08 +0200

    two new fitness functions

--- src/Model/solution.ts
+++ src/Model/solution.ts
@@ -46,7 +46,7 @@ export class Solution {
                 actualJobMachineAssignment[actualMachineIndex].pushJob(Solution.benchmarkReader.jobs[jobIndex]);
             }
             for (let i = 0; i < actualJobMachineAssignment.length; i++) {
-                actualFitnessValue = actualFitnessValue + actualJobMachineAssignment[i].dueDate;
+                actualFitnessValue = actualFitnessValue + actualJobMachineAssignment[i].tardiness;
             }
             if (actualFitnessValue < minFitnessValue) {
                 minFitnessValue = actualFitnessValue;
@@ -59,45 +59,87 @@ export class Solution {
         this.fitnessValue = minFitnessValue;
         return this.fitnessValue;
     }
+    fitness(): number {
+        this.fitnessValue = 0;
+        this.machines = [];
+        // create an empty machines vector
+        for (let m = 0; m < Solution.benchmarkReader.machines.length; m++) {
+            this.machines.push(new Machine((m + 1) + ''));
+        }
+
+        let machineIndex = 0;
+        for (let i = 0; i < this.permutation.length; i++) {
+            const jobIndex = this.permutation[i];
+
+            this.machines[machineIndex].pushJob(Solution.benchmarkReader.jobs[jobIndex]);
+
+            machineIndex++;
+            if (machineIndex == Solution.benchmarkReader.machines.length) {
+                machineIndex = 0;
+            }
+        }
+
+        for (let i = 0; i < this.machines.length; i++) {
+            this.fitnessValue = this.fitnessValue + this.machines[i].tardiness;
+        }
+
+        return this.fitnessValue;
+    }
     //this fitness function is in use
     //this fintess calculation strategy needs a bit modification
     //in order to reach the algorithm the best known solution of the benchmark data
-    fitness(): number {
+    fitness3(): number {
 
         this.fitnessValue = 0;
         let actualMachineIndex = 0;
         this.machines = [];
-        this.machines.push(new Machine((actualMachineIndex + 1) + ''));
 
+        // create an empty machines vector
+        for (let m = 0; m < Solution.benchmarkReader.machines.length; m++) {
+            this.machines.push(new Machine((m + 1) + ''));
+        }
+
+        let isMoreFreeTime = true;
         for (let i = 0; i < this.permutation.length; i++) {
-            let jobIndex = this.permutation[i];
-            let freeTime = this.machines[actualMachineIndex].actualFreeTime + Solution.benchmarkReader.jobs[jobIndex].processingTime;
-            //if due date constraint is met, push the job
-            if (freeTime < Solution.benchmarkReader.jobs[jobIndex].dueDate) {
-                this.machines[actualMachineIndex].pushJob(Solution.benchmarkReader.jobs[jobIndex]);
-            }
-            //if due date constraint is not met, create a new machine (if can) and assing the job to the machine
-            else if (actualMachineIndex < Solution.benchmarkReader.machines.length - 1) {
-                actualMachineIndex++;
-                this.machines.push(new Machine((actualMachineIndex + 1) + ''));
-                this.machines[actualMachineIndex].pushJob(Solution.benchmarkReader.jobs[jobIndex]);
-            }
-            //assign the job to the machine, but it has due date
-            else {
-                let minActualFreeTimeIndex = 0;
-                //search for a machine, which has the minimal jobs time (actual free time value)
-                for (let j = 1; j < this.machines.length; j++) {
-                    if (this.machines[j].actualFreeTime < this.machines[minActualFreeTimeIndex].actualFreeTime) {
-                        minActualFreeTimeIndex = j;
+            const jobIndex = this.permutation[i];
+
+            // select the machine where the free time is the minimum and no due date -> assign job to it
+            let minimumFreetimeMachineIndex = -1;
+            if (isMoreFreeTime) {
+                let miniumFreeTime = -1;
+                for (let m = 0; m < this.machines.length; m++) {
+                    if (this.machines[m].tardiness == 0) {
+                        const actualFreeTime = this.machines[m].time;
+                        if (actualFreeTime > miniumFreeTime) {
+                            miniumFreeTime = actualFreeTime;
+                            minimumFreetimeMachineIndex = m;
+                        }
                     }
                 }
-                this.machines[minActualFreeTimeIndex].pushJob(Solution.benchmarkReader.jobs[jobIndex]);
+                if (minimumFreetimeMachineIndex == -1) {
+                    isMoreFreeTime = false;
+                }
             }
-
+            // we have at least one machine where there is no due date
+            if (isMoreFreeTime) {
+                this.machines[minimumFreetimeMachineIndex].pushJob(Solution.benchmarkReader.jobs[jobIndex]);
+            } else {
+                let maxFreeTime = -1000000;
+                let minDueDateMachineIndex = 0;
+                for (let m = 0; m < this.machines.length; m++) {
+                    let currentFreeTime = Solution.benchmarkReader.jobs[jobIndex].dueDate -
+                        (this.machines[m].time + Solution.benchmarkReader.jobs[jobIndex].processingTime);
+                    if (currentFreeTime > maxFreeTime) {
+                        maxFreeTime = currentFreeTime;
+                        minDueDateMachineIndex = m;
+                    }
+                }
+                this.machines[minDueDateMachineIndex].pushJob(Solution.benchmarkReader.jobs[jobIndex]);
+            }
         }
 
         for (let i = 0; i < this.machines.length; i++) {
-            this.fitnessValue = this.fitnessValue + this.machines[i].dueDate;
+            this.fitnessValue = this.fitnessValue + this.machines[i].tardiness;
         }
         return this.fitnessValue;
     }
