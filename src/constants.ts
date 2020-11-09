93f8d4a0b2240be65111fc9b55c0e0a388cd60f6
Karoly Nehez <aitnehez@uni-miskolc.hu>
2020-11-09 12:42:08 +0100

    code review

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,11 @@
 export const ONE_BENCHMARK_TANAKA = "benchmark_instances/Tanaka/instances/n=20/m=2/20_02_02_02_001.dat";
 
+// only one test
+//export const BENCHMARKS_INSTANCES_PATH_ERIC = "benchmark_instances/Eric/small_test/";
+
 // full test of all the 2500 instances
 export const BENCHMARKS_INSTANCES_PATH_TANAKA = "benchmark_instances/Tanaka/instances/";
 export const BENCHMARKS_INSTANCES_PATH_ERIC = "benchmark_instances/Eric/instances/";
-// only one test
-//export const BENCHMARKS_INSTANCES_PATH = "benchmark_instances/small_test/";
+
 export const BENCHMARK_RESULTS_PATH_TANAKA = "benchmark_instances/Tanaka/results";
 export const BENCHMARK_RESULTS_PATH_ERIC = "benchmark_instances/Eric/results";
-//not in use
-export const OUTPUT_PATH = "";
